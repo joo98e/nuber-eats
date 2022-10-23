ecef40fdd0b2a0749fcb6be8fd7dc06e3705c0ab
joo98e <jtbeok@gmail.com>
2022-10-23 19:16:15 +0900

    feat : cross-env를 통해 env 변수 설정 (prod는 추후 배포 시 적용)

--- dist/app.module.js
+++ dist/app.module.js
@@ -10,12 +10,30 @@ exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
 const graphql_1 = require("@nestjs/graphql");
 const apollo_1 = require("@nestjs/apollo");
+const typeorm_1 = require("@nestjs/typeorm");
+const config_1 = require("@nestjs/config");
 const restaurants_module_1 = require("./restaurants/restaurants.module");
 let AppModule = class AppModule {
 };
 AppModule = __decorate([
     (0, common_1.Module)({
         imports: [
+            config_1.ConfigModule.forRoot({
+                cache: false,
+                encoding: "utf8",
+                isGlobal: true,
+                envFilePath: ".env",
+            }),
+            typeorm_1.TypeOrmModule.forRoot({
+                type: "postgres",
+                host: "localhost",
+                port: 5432,
+                username: "joo98e",
+                password: "로컬 호스트로 연결 시 비밀번호를 검증하지 않는다. 12345",
+                database: "nuber-eats",
+                synchronize: true,
+                logging: true,
+            }),
             graphql_1.GraphQLModule.forRoot({
                 autoSchemaFile: true,
                 driver: apollo_1.ApolloDriver,
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
-import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { RestaurantsModule } from '@modules/restaurants/restaurants.module';
+import { Module } from "@nestjs/common";
+import { GraphQLModule } from "@nestjs/graphql";
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule } from "@nestjs/config";
+
+import { RestaurantsModule } from "@modules/restaurants/restaurants.module";
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      cache: false,
+      encoding: "utf8",
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === "dev" ? ".env.dev" : ".env.test",
+    }),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "joo98e",
+      password: "로컬 호스트로 연결 시 비밀번호를 검증하지 않는다. 12345",
+      database: "nuber-eats",
+      synchronize: true,
+      logging: true,
+      // entities: [3000, Category],
+      // subscribers: [],
+      // migrations: [],
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       autoSchemaFile: true,
       driver: ApolloDriver,
